034d1b904464701999dc25a1499eb20c818e655f
justinbourb <justinbourb@gmail.com>
2018-01-02 18:43:40 -0500

    end of chapter 7

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,4 +14,29 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
+
+if not app.debug:
+    '''commented out section below is for setting up
+    error reporting by email, which I don't care to use
+    at this time 1.2.18 JB see chapter 7.4 in flask mega tutorial'''
+    '''if app.config['MAIL_SERVER']:
+        auth = None
+    secure = None
+    if app.config['MAIL_USE_TLS']:
+        secure = ()
+    mail_handler = SMTPHandler(
+    mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+    fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+    toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+    credentials=auth, secure=secure)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)'''
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler   (file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
