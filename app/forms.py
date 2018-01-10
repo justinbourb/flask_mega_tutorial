a0657d809438e3d475eecebc79541872759610a2
justinbourb <justinbourb@gmail.com>
2018-01-09 20:41:55 -0500

    end of chapter 10

--- app/forms.py
+++ app/forms.py
@@ -7,6 +7,7 @@ from app.models import User
 
 
 class LoginForm(FlaskForm):
+    """ login registered users"""
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
@@ -14,6 +15,7 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
+    """ register new users"""
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -22,17 +24,20 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Register')
 
     def validate_username(self, username):
+        """ validates user name not taken """
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Please use a different username.')
 
     def validate_email(self, email):
+        """ validate email not taken"""
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
 
 
 class EditProfileForm(FlaskForm):
+    """ edit user profiles"""
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
@@ -42,11 +47,24 @@ class EditProfileForm(FlaskForm):
         self.original_username = original_username
 
     def validate_username(self, username):
+        """ validates user name not taken"""
         if username.data != self.original_username:
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Please use a different username.')
 
 class PostForm(FlaskForm):
+    """ create blog posts"""
     post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
+
+class ResetPasswordRequestForm(FlaskForm):
+    """request to reset the user password"""
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    """ resets the user password"""
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from hashlib import md5
-from app import db, login
+from time import time
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from app import app, db, login
+
 
 
 followers = db.Table(
@@ -30,35 +33,57 @@ class User(UserMixin, db.Model):
         return '<User {}>'.format(self.username)
 
     def set_password(self, password):
+        """generate password hash"""
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
+        """checks password hash"""
         return check_password_hash(self.password_hash, password)
 
     def avatar(self, size):
+        """set up gravatar"""
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
     def follow(self, user):
+        """follow a user"""
         if not self.is_following(user):
             self.followed.append(user)
 
     def unfollow(self, user):
+        """un follow a user"""
         if self.is_following(user):
             self.followed.remove(user)
 
     def is_following(self, user):
+        """counts number of followers"""
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
+        """shows posts the user follows"""
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
+    def get_reset_password_token(self, expires_in=600):
+        """creates jwt token for resetting password"""
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        """verifies reset password token"""
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 @login.user_loader
 def load_user(id):
